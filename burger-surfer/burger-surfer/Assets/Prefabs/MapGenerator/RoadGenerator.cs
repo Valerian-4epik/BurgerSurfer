1bdcf42485d612bd535a80cd63bf136986ba4a18
Valerian-4epik <98113328+Valerian-4epik@users.noreply.github.com>
2022-09-26 13:27:41 +0300

    add roadGenerator

--- burger-surfer/burger-surfer/Assets/Prefabs/MapGenerator/RoadGenerator.cs
+++ burger-surfer/burger-surfer/Assets/Prefabs/MapGenerator/RoadGenerator.cs
@@ -5,18 +5,29 @@ using Random = System.Random;
 
 public class RoadGenerator : MonoBehaviour
 {
-    [SerializeField] private List<GameObject> _roadPrefabs;
+    [SerializeField] private List<GameObject> _firstPath;
+    [SerializeField] private List<GameObject> _secondPath;
+    [SerializeField] private List<GameObject> _thirdPath;
+    [SerializeField] private List<GameObject> _fourthPath;
+    [SerializeField] private List<GameObject> _fifthPath;
+    [SerializeField] private List<GameObject> _sixthPath;
     [SerializeField] private GameObject _finish;
-    [SerializeField] private int _roadLength;
 
+    private List<GameObject> _roadPath = new List<GameObject>();
     private BoxCollider _boxCollider;
     private List<GameObject> _road = new List<GameObject>();
-    private Vector3 _finishRotation = new Vector3(0,180,0);
+    //private Vector3 _finishRotation = new Vector3(0,180,0);
     private float _pathWidth;
     private Random _randomPath = new Random();
-    
+
     private void Start()
     {
+        _roadPath.Add(GetRandomPart(_firstPath));
+        _roadPath.Add(GetRandomPart(_secondPath));
+        _roadPath.Add(GetRandomPart(_thirdPath));
+        _roadPath.Add(GetRandomPart(_fourthPath));
+        _roadPath.Add((GetRandomPart(_fifthPath)));
+        _roadPath.Add(GetRandomPart(_sixthPath));
         CreateRoad();
     }
 
@@ -25,7 +36,7 @@ public class RoadGenerator : MonoBehaviour
         Vector3 position = Vector3.zero;
         //Vector3 finishWidth = new Vector3(0, 0, 9);
 
-        for (int i = 0; i < _roadLength; i++)
+        for (int i = 0; i < _roadPath.Count; i++)
         {
             Vector3 width = new Vector3(_pathWidth, 0, 0);
             
@@ -34,7 +45,7 @@ public class RoadGenerator : MonoBehaviour
                 position = _road[_road.Count - 1].transform.position + width;
             }
             
-            var road = Instantiate(GetRandomPart(), position, Quaternion.identity);
+            var road = Instantiate(_roadPath[i], position, Quaternion.identity);
             _pathWidth = road.GetComponent<BoxCollider>().bounds.size.x;
             road.transform.SetParent(transform);
             _road.Add(road);
@@ -42,9 +53,9 @@ public class RoadGenerator : MonoBehaviour
         //GameObject finish = Instantiate(_finish, position + finishWidth, Quaternion.Euler(_finishRotation), transform);
     }
 
-    private GameObject GetRandomPart()
+    private GameObject GetRandomPart(List<GameObject> list)
     {
-        int randomNumber = _randomPath.Next(0, _roadPrefabs.Count);
-        return _roadPrefabs[randomNumber].gameObject;
+        int randomNumber = _randomPath.Next(0, list.Count);
+        return list[randomNumber].gameObject;
     }
 }
