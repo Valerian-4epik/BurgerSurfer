ae577376801c73d662a5b0f44e6b3eb903ab376f
Valerian-4epik <98113328+Valerian-4epik@users.noreply.github.com>
2022-10-18 22:40:57 +0300

    add yandex sdl

--- burger-surfer/burger-surfer/Assets/Scripts/Services/SceneLoader.cs
+++ burger-surfer/burger-surfer/Assets/Scripts/Services/SceneLoader.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections;
+using Agava.YandexGames;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,8 +8,20 @@ namespace Scripts.Services.SceneLoader
 {
     public class SceneLoader : MonoBehaviour
     {
+        private IEnumerator Awake()
+        {
+#if !UNITY_WEBGL || UNITY_EDITOR
+            yield break;
+#endif
+            yield return YandexGamesSdk.Initialize();
+            
+            DontDestroyOnLoad(this);
+        }
+
         public void RestartScene()
         {
+            VideoAd.Show();
+
             ES3AutoSaveMgr.Current.Save();
             SceneManager.LoadScene(0);
         }
